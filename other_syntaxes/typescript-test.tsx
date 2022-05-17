81ea33c44544818cd9918ab039474fde138ebe33
Alvin Ramskogler <62756994+blaumeise20@users.noreply.github.com>
2022-05-17 21:23:53 +0200

    Initial commit
    Add some basic syntax highlighting.

---
+++ other_syntaxes/typescript-test.tsx
@@ -0,0 +1,13 @@
+<div class={"helo"} asdf:hi="e">
+    elo
+    {"test"}
+    <Easdf.A />
+</div>
+;
+`asdf${'test' + 123}`
+
+class Test {
+    constructor(public name: string) {
+        
+    }
+}
